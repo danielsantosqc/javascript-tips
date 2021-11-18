e6e651d75e9aaf17f4c505ffd2d28d7b82151b13
Daniel Quispe <danielsantosqc@gmail.com>
2021-11-18 20:47:30 -0300

    terminado

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const elemento = document.getElementById("contenido");
+// console.log("😎😎😎");
+
+// document.addEventListener("DOMContentLoaded", () => {  
+//  contenido
+  // })
+
+// console.log(document.getElementById("tituloWeb"));
+// console.log(document.getElementById("tituloWeb").textContent);
+
+const h1 = document.getElementById("tituloWeb");
+console.log(h1.className);
+console.log(h1.id);
+console.log(h1.textContent);
+console.log(h1.tagName);
+console.log(h1.style);
+h1.textContent = "Nuevo texto desde JS";
+h1.style.backgroundColor = "green"; 
+h1.style.color = "white";
+
+// ------
+const tituloWeb = document.getElementById("tituloWeb");
 const botonR = document.getElementById("btn-color-red");
-const botonG = document.getElementById("btn-color-green");
+const botonB = document.getElementById("btn-color-green");
 
 botonR.addEventListener("click", () => {
   console.log("hiciste click");
-  elemento.style.color = "red";
+  tituloWeb.style.color = "red";
 })
 
-botonG.addEventListener("click", () =>{
-  elemento.style.color = "green";
+botonB.addEventListener("click", () =>{
+  tituloWeb.style.color = "black";
 })
