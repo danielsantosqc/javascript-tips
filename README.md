5787ad331ed322c78224ba71215b72c537b959fe
Daniel Quispe <danielsantosqc@gmail.com>
2021-12-04 19:04:50 -0300

    Hasta aqui funciona bien, solo falta que funcione el boton Finalizar compra

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # javascript-tips
-carrito de compra modificamos de objeto para array, para manejo diferente y mas efectivo para este caso.
+
+# Ejemplo carrito de compras con objeto, template, templateFooter.
+
+
+## template
+```html
+<template id="template">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span class="lead">Frutilla</span>
+        <span class="badge bg-primary rounded-pill">12</span>
+    </li>
+</template>
+````
+
+## template Footer
+```html
+<template id="templateFooter">            
+      <div class="card">
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <p class="lead mb-0">Total $ <span>1500</span></p>
+          <button class="btn btn-outline-info">Finalizar Compra</button>
+        </div>
+      </div>
+    </template>
--- practice2.js
+++ practice2.js
@@ -1,13 +1,17 @@
 const carrito = document.getElementById("carrito");
 const template = document.getElementById("template");
-const mainFooter = document.getElementById("main-footer");
+const mainFooter = document.getElementById("mainFooter");
+const templateFooter = document.getElementById("templateFooter");
 const fragment = document.createDocumentFragment();
 const btnsBotones = document.querySelectorAll(".card .btn");
 // console.log(carrito);
 // console.log(template);
+// console.log(mainFooter);
 // console.log(fragment);
 // console.log(btnsBotones);
 
+
+// Aqui se captura los eventos del DOM para el funcionamiento del sitio
 document.addEventListener("click",(e) => {
   // console.log(e.target.matches(".card .btn-outline-primary"));
   if(e.target.matches(".card .btn-outline-primary")){
@@ -18,17 +22,21 @@ document.addEventListener("click",(e) => {
   // console.log(e.target.matches("#carrito .list-group-item .btn-danger"));
   if(e.target.matches("#carrito .list-group-item .btn-danger")){
     console.log("quitar");
+    btnDisminuir(e);
   }
 
   // console.log(e.target.matches("#carrito .list-group-item .btn-success"));
   if(e.target.matches("#carrito .list-group-item .btn-success")){
     console.log("agregar");
+    btnAumentar(e);
   }
+
   
 })
 
-const carritoObjeto = [];
+let carritoObjeto = [];
 
+// agregando al carrito----------
 const agregarAlCarrito = (e) => {
   // console.log(e.target.dataset.fruta);
   const producto = {
@@ -36,6 +44,7 @@ const agregarAlCarrito = (e) => {
     id: e.target.dataset.fruta,
     cantidad: 1,
     precio: parseInt(e.target.dataset.precio),
+    precioTotal:parseInt(e.target.dataset.precio)
   };
   // console.log(producto);
 
@@ -45,11 +54,56 @@ const agregarAlCarrito = (e) => {
     carritoObjeto.push(producto);
   }else {
     carritoObjeto[indice].cantidad++;
-    carritoObjeto[indice].precio = carritoObjeto[indice].cantidad * producto.precio;
+    carritoObjeto[indice].precioTotal = carritoObjeto[indice].cantidad * producto.precio;
   }
   console.log(carritoObjeto);
   pintarCarrito(carritoObjeto);
-  // console.log(carritoObjeto);
+};
+
+// aumentar al  carrito-----------
+const btnAumentar = (e)=>{
+  console.log("aumentaste ",e.target.dataset.fruta);
+  carritoObjeto = carritoObjeto.map(item => {
+    if(item.id === e.target.dataset.fruta){
+      item.cantidad ++
+      item.precioTotal = item.precio * item.cantidad; 
+    }
+    return item;
+  });
+  pintarCarrito(carritoObjeto);
+};
+
+// disminuir carrito -------
+const btnDisminuir = (e)=>{
+  console.log("disminuiste ",e.target.dataset.fruta);
+  carritoObjeto = carritoObjeto.filter(item => {
+    if(item.id === e.target.dataset.fruta){
+      if(item.cantidad > 0){
+        item.cantidad --;
+        item.precioTotal = item.precio * item.cantidad; 
+        if(item.cantidad === 0) return
+        return item;
+      }
+    }else{
+      return item;
+    }
+  });
+  pintarCarrito(carritoObjeto);
+}
+
+const pintarFooter = () =>{
+  console.log("Pintarfooter");
+  mainFooter.textContent = "";
+  if(carritoObjeto.length !== 0){  
+    const total = carritoObjeto.reduce(
+      (acc, current) => acc + current.cantidad * current.precio, 0
+      );  
+      const clone = templateFooter.content.cloneNode(true);
+      clone.querySelector('span').textContent = total;
+      mainFooter.appendChild(clone);
+  }else{
+    mainFooter.textContent = "";
+  }
 };
 
 const pintarCarrito = (array) =>{
@@ -59,11 +113,17 @@ const pintarCarrito = (array) =>{
     const clone = template.content.cloneNode(true);
     clone.querySelector('.lead').textContent = item.titulo;
     clone.querySelector('.badge').textContent = item.cantidad;
-    clone.querySelector('div .lead span').textContent = item.precio;
+    clone.querySelector('div .lead span').textContent = item.precioTotal;
+
+    // buttons---
+    clone.querySelector('.btn-danger').dataset.fruta = item.id;
+    clone.querySelector('.btn-success').dataset.fruta = item.id;
 
     fragment.appendChild(clone);
   });
   carrito.appendChild(fragment);
+
+  pintarFooter();
 };
 
 // btnsBotones.forEach((btn) => {
