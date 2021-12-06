ffd416347d22d35a01ad602df4f3b37bc229ba14
Daniel Quispe <danielsantosqc@gmail.com>
2021-12-06 20:17:10 -0300

    formulario y expresiones regulares usados en el

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const elemento = document.getElementById("contenido");
-const botonR = document.getElementById("btn-color-red");
-const botonG = document.getElementById("btn-color-green");
+// const regExp = /daniel/i;
+// const regExpObjeto = new RegExp("daniel", "i");
+// console.log(regExp.test("dan dan"));
 
-botonR.addEventListener("click", () => {
-  console.log("hiciste click");
-  elemento.style.color = "red";
-})
+const regExpName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regExpEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
 
-botonG.addEventListener("click", () =>{
-  elemento.style.color = "green";
-})
+
+const formulario = document.getElementById("formulario");
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+
+
+
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(userName.value);
+  console.log(userEmail.value);
+
+  if(!regExpName.test(userName.value)){
+    console.log("Formato de nombre no valido");
+  }
+  if(!regExpEmail.test(userEmail.value)){
+    console.log("Formato de email no valido");
+  }
+
+});
