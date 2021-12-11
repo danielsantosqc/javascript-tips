4c865b2ef9072f4fa62045b5a127977cbb06bb06
Daniel Quispe <danielsantosqc@gmail.com>
2021-12-10 22:25:53 -0300

    init project

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const elemento = document.getElementById("contenido");
-const botonR = document.getElementById("btn-color-red");
-const botonG = document.getElementById("btn-color-green");
+// console.log("Funcionando");
 
-botonR.addEventListener("click", () => {
-  console.log("hiciste click");
-  elemento.style.color = "red";
-})
+// const url = "https://jsonplaceholder.typicode.com/posts/";
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-botonG.addEventListener("click", () =>{
-  elemento.style.color = "green";
-})
+// primera forma
+// fetch(url)
+//   .then((res) => res.json())
+//   .then(data => {
+//     // Object.entries(data).forEach(([key, value]) =>{
+//     //   console.log(key);
+//     //   Object.entries(value).forEach(([llave, valor]) => {
+//     //     console.log(llave+ " => " +valor);
+//     //   });
+//     //   console.log("---------------");
+//     // });
+//      console.log(data)
+//   })
+//   .catch(e =>console.log(e))
+//   .finally(()=> console.log("Finalizo"));
+
+
+
+// segunda forma 
+ const findPostById = async (id) =>{
+   try {
+     const res = await fetch(url);
+     const post = await res.json();
+     console.log(post);
+   } catch (error) {
+     console.log(error);
+   }
+ }
+
+ findPostById();
