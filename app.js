4c95ef68fa2c84b0088c0a95978591435a2edf2b
Daniel Quispe <danielsantosqc@gmail.com>
2021-12-19 17:39:33 -0300

    Hasta aqui, trajimos los datos de la api y la mostramos en la pagina

--- app.js
+++ app.js
@@ -29,12 +29,20 @@ const fetchData = async() => {
 const pintarCards = (data) => {
   // console.log(data);
   const cards = document.getElementById("card-dinamicas");
-  const templateCard = document.getElementById("template-card");
-  data.results.forEach(element => {
-    console.log(element);
+  const templateCard = document.getElementById("template-card").content;
+  const fragment = document.createDocumentFragment();
+  data.results.forEach((item) => {  
+    console.log(item);
+
     const clone = templateCard.cloneNode(true);
-    clone.querySelector
+    clone.querySelector("h5").textContent  = item.name
+    clone.querySelector("p").textContent = item.species;
+    clone.querySelector(".card-img-top").setAttribute("src", item.image);
+
+    fragment.appendChild(clone);
+    
   });
+  cards.appendChild(fragment);
 }
 
 const loadingData = estado => {
