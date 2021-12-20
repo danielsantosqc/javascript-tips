0e5dd7a39762c5378a19e1247a08a7c1b6cb915e
Daniel Quispe <danielsantosqc@gmail.com>
2021-12-20 00:37:44 -0300

    termine el proyecto y esta funcionando perfectamente, tube que sacar el evento fuera de afuera de cualquier funcion para que deje de dar error

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
-
-
 document.addEventListener("DOMContentLoaded",() =>{
   fetchData();
 });
+let guardaUrl = [];
+let contadorDeGets = 1;
+let contador = 1;
 
-const fetchData = async() => {
+// inicio  de eventos de los botones prev y next
+const botonPrev = document.querySelector('.btn-outline-secondary');
+const contadorDePagina = document.querySelector('.contador');
+const botonNext = document.querySelector('.btn-outline-primary');
+
+botonPrev.addEventListener("click", (e) =>{
+  if(guardaUrl.info.prev){
+    console.log(guardaUrl.info.prev);
+    fetchData(guardaUrl.info.prev);
+    contador --;
+    contadorDePagina.textContent = contador;
+  }
+  // alert("Atras");
+});
+botonNext.addEventListener("click", (e) =>{
+  if(guardaUrl.info.next){
+    console.log(guardaUrl.info.next);
+    fetchData(guardaUrl.info.next);
+    contador++;
+    contadorDePagina.textContent = contador;
+  }
+  // alert("Siguiente");
+});
+// fin de eventos
+
+
+const fetchData = async(url = "https://rickandmortyapi.com/api/character" ) => {
   
   try {
-    console.log("Obteniendo Datos");
+    console.log("Obteniendo Datos "+ contadorDeGets +"⁰ vez");
+    contadorDeGets ++;
+
     loadingData(true);
-    let res =  await fetch ("https://rickandmortyapi.com/api/character");  
+    const res =  await fetch ( url);  
     const data = await res.json();
     // console.log(data);
+    guardaUrl = data;
+    pintarPaginacion(data);
     pintarCards(data);
 
-    // window.setTimeout(function (){
-    //   console.log("Holaaaaa.....");
-    // },2000);
-    
   } catch (error) {
     console.log(error);
   }finally{
@@ -26,19 +53,44 @@ const fetchData = async() => {
   }
 }
 
+
+const pintarPaginacion = (data) =>{
+  // console.log(data.info);
+  const botonPrev = document.querySelector('.btn-outline-secondary');
+  const botonNext = document.querySelector('.btn-outline-primary');
+
+  if (!data.info.prev){
+    botonPrev.disabled = true;
+  }else{
+    botonPrev.style.backgroundColor = "blue";
+    botonPrev.disabled = false;
+    // console.log("preview: ", data.info.prev);
+  }
+
+  if(!data.info.next){
+    botonNext.disabled = true;
+  }else{
+    botonNext.style.backgroundColor = "blue";
+    botonNext.disabled = false;
+    // console.log("next: ", data.info.next);
+
+  }
+} 
+
 const pintarCards = (data) => {
-  // console.log(data);
+  console.log(data.results);
   const cards = document.getElementById("card-dinamicas");
+  cards.textContent = ""
   const templateCard = document.getElementById("template-card").content;
   const fragment = document.createDocumentFragment();
   data.results.forEach((item) => {  
-    console.log(item);
-
+    // console.log(item.name);
     const clone = templateCard.cloneNode(true);
     clone.querySelector("h5").textContent  = item.name
     clone.querySelector("p").textContent = item.species;
     clone.querySelector(".card-img-top").setAttribute("src", item.image);
 
+    // guardamos en el fragment para evitar el reflow
     fragment.appendChild(clone);
     
   });
@@ -46,6 +98,7 @@ const pintarCards = (data) => {
 }
 
 const loadingData = estado => {
+
   const loading = document.getElementById("loading");
   if(estado){
     loading.classList.remove("d-none");
