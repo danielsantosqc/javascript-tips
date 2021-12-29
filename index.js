59220b92eb0481c4bbc2fbf50cd75c7025cfecc0
Daniel Quispe <danielsantosqc@gmail.com>
2021-12-29 20:23:27 -0300

    Init .....

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const elemento = document.getElementById("contenido");
-const botonR = document.getElementById("btn-color-red");
-const botonG = document.getElementById("btn-color-green");
+// LO QUE ANTES HABIA
+// function Persona (nombre){
+//   this.nombre = nombre,
+//   this.saludar = function saludar (){
+//     return ` ${nombre} dice saludar`;
+//   }
 
-botonR.addEventListener("click", () => {
-  console.log("hiciste click");
-  elemento.style.color = "red";
-})
+// }
 
-botonG.addEventListener("click", () =>{
-  elemento.style.color = "green";
-})
+// // Un prototypo es un metodo que se le puede concatenar a la lesta de prototipos del objeto en cuestion
+// Persona.prototype.saludarEnIngles = function(){
+//   return `Hi my name is ${this.nombre}`;
+// }
+// const juanito = new Persona ("juanitooooo");
+// const pedrito = new Persona ("Pedritoooo");
+
+// console.log(juanito);
+// console.log(pedrito);
+// console.log(juanito.saludar());
+// console.log(pedrito.saludar());
+
+
+
+// ==============LO NUEVO ACTUALIZADO EN ECMAScript 2015
+//solo muda la parte visual  con la palabra class, por dentro es lo mismo
+
+class Persona{
+  constructor(nombre){
+    this._nombre = nombre;
+  }
+
+  get nombre () {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+
+
+  saludar(){
+    return `${this.nombre} dice holaaaa...`
+  }
+}
+
+const juanito = new Persona ("Juanito");
+juanito.nombre = "Juan";
+console.log(juanito.saludar());
