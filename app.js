6059d64ca6bcb4d7563bf577c8676d57a50786dc
Daniel Quispe <danielsantosqc@gmail.com>
2022-01-09 22:49:32 -0300

    update

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Provando");
+// console.log("Provando");
+const formulario = document.querySelector('#formulario');
+const cardsEstudiantes = document.querySelector('#cardsEstudiantes');
+const cardsProfesores = document.querySelector('#cardsProfesores');
+const templateEstudiante = document.querySelector('#templateEstudiante').content;
+const templateProfesor = document.querySelector('#templateProfesor').content;
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const datos = new FormData (formulario);
+  // console.log(datos);
+  // console.log([...datos.values()]);
+  const [nombre, edad, opcion] = [...datos.values()];
+  console.log(nombre, edad, opcion);
+});
