cf13ba7ea7ae65eabc7a26284c47e6dc900bc411
Daniel Quispe <danielsantosqc@gmail.com>
2022-01-19 07:48:15 -0600

    todo listo

--- app.js
+++ app.js
@@ -31,13 +31,33 @@ const agregarTodo = todo => {
 }
 
 const pintarTodos = () => {
+  localStorage.setItem("todoLocalStorage",JSON.stringify(arrayTodos))
   pintarTodo.textContent = "";
   const fragment = document.createDocumentFragment();
 
   arrayTodos.forEach(item => {
     const clone = templateTodo.cloneNode(true);
     clone.querySelector(".lead").textContent = item.nombre;
+    clone.querySelector(".btn").dataset.id = item.id;
+
     fragment.appendChild(clone);
   });
   pintarTodo.appendChild(fragment);
 }
+
+document.addEventListener('click',(e) => {
+  // console.log(e.target.dataset.id);
+  // console.log(e.target.matches(".btn-danger"));
+
+  if (e.target.matches(".btn-danger")){
+    arrayTodos = arrayTodos.filter(item => item.id !== e.target.dataset.id);
+    pintarTodos();
+  }  
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  if (localStorage.getItem("todoLocalStorage")) {
+    arrayTodos = JSON.parse(localStorage.getItem("todoLocalStorage"));
+    pintarTodos();
+  }  
+});
